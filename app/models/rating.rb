983aeedc78ec6ecaf71673655e4a1f11284b6c15
Amar Shah <amar47shah@gmail.com>
2016-02-20 13:05:08 -0500

    Show beverage name and user name instead of objects

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -5,4 +5,7 @@ class Rating < ActiveRecord::Base
   validates_presence_of :user
   validates_presence_of :beverage
   validates_inclusion_of :value, :in => 1..5
+
+  delegate :name, prefix: true, to: :user
+  delegate :name, prefix: true, to: :beverage
 end

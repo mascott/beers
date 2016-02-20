81d54b006e86ee34c6199ccefb221ee9930bb2bf
Amar Shah <amar47shah@gmail.com>
2016-02-19 22:04:58 -0500

    Remove UPC length validation
    At the moment, it is not yet certain how much or precisely what
    length validation is needed for the upc field.

--- app/models/beverage.rb
+++ app/models/beverage.rb
@@ -2,12 +2,5 @@ class Beverage < ActiveRecord::Base
   has_many :ratings
   has_many :users, through: :ratings
 
-  validate :upc_length
   validates_presence_of :upc
-
-  def upc_length
-    unless upc.size == 8 or upc.size == 12
-      errors.add(:upc, "length must be 8 or 12")
-    end
-  end
 end
--- test/models/beverage_test.rb
+++ test/models/beverage_test.rb
@@ -4,16 +4,4 @@ class BeverageTest < ActiveSupport::TestCase
   test 'validates presence' do
     validate_presence_of :upc
   end
-
-  context 'with a 8 character upc' do
-    should allow_value('12345678').for(:upc)
-  end
-
-  context 'with a 12 character upc' do
-    should allow_value('123456789012').for(:upc)
-  end
-
-  context 'with an invalid upc character count' do
-    should_not allow_value('12345678901').for(:upc)
-  end
 end
